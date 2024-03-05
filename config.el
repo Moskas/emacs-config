712388caa5126d12c95f84c3131baca22efc51a3
Moskas <minemoskas@gmail.com>
2024-03-05 22:26:27 +0100

    Some tweaking and eye candy changes

--- config.el
+++ config.el
@@ -33,10 +33,10 @@
 (setq fancy-splash-image (logo-random))
 
 (setq doom-theme 'doom-gruvbox)
-(add-to-list 'default-frame-alist '(background-color . "#32302f"))
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'medium)
+;;(add-to-list 'default-frame-alist '(background-color . "#32302f"))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'regular)
       ;;doom-unicode-font(font-spec :family "JetBrainsMono Nerd Font" :size 16)
-      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'medium))
+      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 16 :weight 'regular))
 (global-prettify-symbols-mode 1)
 (beacon-mode 1) ;; Flash line on cursor movement
 
@@ -54,7 +54,7 @@
   :defer 20
   :custom
   (blamer-idle-time 0.3)
-  (blamer-min-offset 70)
+  (blamer-min-offset 20)
   :custom-face
   (blamer-face ((t :foreground "#d3869b"
                    :background nil
@@ -62,15 +62,10 @@
                    :italic t)))
   :config
   (global-blamer-mode 1))
-;;(setq blamer-view 'overlay)
-;;(setq blamer-author-formatter "  ✎ %s ")
-;;(setq blamer-datetime-formatter "[%s]")
-;;(setq blamer-commit-formatter " ● %s")
-
 ;; Nov-mode setup
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 (defun my-nov-font-setup ()
-  (face-remap-add-relative 'variable-pitch :family "Iosevka"
+  (face-remap-add-relative 'variable-pitch :family "JetBrains Mono Nerd Font"
                            :height 1.0
                            :weight medium))
 (setq nov-text-width t)
@@ -92,33 +87,37 @@
 ;; Org-mode tweaks
 (defun my-org-faces ()
   (set-face-attribute 'org-todo nil :height 1.0)
-  (set-face-attribute 'org-level-1 nil :height 1.2)
-  (set-face-attribute 'org-level-2 nil :height 1.1)
+  (set-face-attribute 'org-level-1 nil :height 1.0)
+  (set-face-attribute 'org-level-2 nil :height 1.0)
+  (set-face-attribute 'org-level-3 nil :height 1.0)
   (setq org-hide-emphasis-markers t)
-  (set-face-attribute 'org-block-begin-line nil :background 'unspecified)
-  (set-face-attribute 'org-block-end-line nil :background 'unspecified)
+  ;;(set-face-attribute 'org-block-begin-line nil :background 'unspecified)
+  ;;(set-face-attribute 'org-block-end-line nil :background 'unspecified)
   ;; Set the foreground color to the value of the background color
-  (set-face-attribute 'org-block-begin-line nil
-                      :foreground (face-background 'org-block-begin-line nil 'default))
-  (set-face-attribute 'org-block-end-line nil
-                      :foreground (face-background 'org-block-end-line nil 'default))
+  ;;(set-face-attribute 'org-block-begin-line nil
+  ;;                    :foreground (face-background 'org-block-begin-line nil 'default))
+  ;;(set-face-attribute 'org-block-end-line nil
+  ;;                    :foreground (face-background 'org-block-end-line nil 'default))
   (display-time)
   (display-line-numbers-mode 0)
-  )
+  (blamer-mode 0)
+  (beacon-mode 0))
 (add-hook 'org-mode-hook #'my-org-faces)
 
 ;; Markdown-mode tweaks
 (custom-set-faces
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.3))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.2))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.1)))))
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.0))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.0)))))
 (add-hook 'python-mode-hook #'rainbow-mode)
 (add-hook 'org-mode-hook #'olivetti-mode)
 (add-hook 'markdown-mode-hook #'olivetti-mode)
 ;; eshell config
 (defun with-face (str &rest face-plist)
   (propertize str 'face face-plist))
+
+(direnv-mode)
 
 (defun shk-eshell-prompt ()
   (let ((header-bg "#282828"))
@@ -131,7 +130,7 @@
      (with-face "\n" :background header-bg)
      (with-face user-login-name :foreground "#458588")
      "@"
-     (with-face "gungnir" :foreground "#689d6a")
+     (with-face (system-name) :foreground "#689d6a")
      (if (= (user-uid) 0)
          (with-face " #" :foreground "#cc241d")
        " $")
@@ -139,19 +138,21 @@
 (setq eshell-prompt-function 'shk-eshell-prompt)
 (setq eshell-highlight-prompt nil)
 
-(direnv-mode)
-
 (use-package ellama
   :init
   (setopt ellama-language "English")
   (require 'llm-ollama)
   (setopt ellama-provider
-	  (make-llm-ollama
-	   :chat-model "codellama" :embedding-model "codellama")))
+          (make-llm-ollama
+           :chat-model "codellama" :embedding-model "codellama")))
 
-(global-set-key (kbd "C-S-V") #'kill-ring-save)
+;;(global-set-key (kbd "C-S-V") #'kill-ring-save)
 
-(custom-set-faces! '(default :background "#282828"))
+(add-hook 'nix-mode-hook #'rainbow-delimiters-mode)
+(setq direnv-always-show-summary nil)
+
+(use-package rainbow-mode
+  :hook org-mode prog-mode)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
